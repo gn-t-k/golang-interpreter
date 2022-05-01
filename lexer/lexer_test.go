072a395c8cfcbe3c65a1c4a4992fd63bf8fd1181
gn-t-k <gntk1995422@gmail.com>
2022-05-01 21:55:20 +0900

    wip

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -58,6 +58,19 @@ let result = add(five, ten);
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		// temp
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
