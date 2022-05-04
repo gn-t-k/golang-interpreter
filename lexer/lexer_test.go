4b3793a5f2e70396bab428e3a44d59086ea3bec0
gn-t-k <gntk1995422@gmail.com>
2022-05-04 21:47:07 +0900

    wip

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,7 +7,8 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `let five = 5;
+	input := `
+let five = 5;
 let ten = 10;
 let add = fn(x, y) {
   x + y;
@@ -21,7 +22,8 @@ if (5 < 10) {
 	return true;
 } else {
 	return false;
-}`
+}
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
